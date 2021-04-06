dcb375e3a5d4d125a6bb1356cbfc5c6e3ea3a46f
marcosxouba <marcosxouba@gmail.com>
2021-04-06 14:51:25 +0200

    Update welcomtext.js

--- welcomtext.js
+++ welcomtext.js
@@ -1,5 +1,17 @@
 const welcome = (number, groupname) => {
-    return `Hola @${number}. Bienvenido al grupo ${groupname} lee las reglas para no tener mal entendidos`
+    return `Hola @${number}. Bienvenido al grupo ${groupname} 
+  ┏━━━━━━━━━━━━━━━━
+  ┃〘 *PRESÉNTATE* 〙
+  ┃━━━━━━━━━━━━━━━━━
+  ┠⊷️ *Nombre* :
+  ┠⊷️ *Edad* :
+  ┠⊷️ *Lugar* :
+  ┗━━━━━━━━━━━━━━━━━
+ ⚠️ SI HACES SPAM TE DEJAMOS SIN WHATSAPP + GRUPO / INSTAGRAM⚠️
+Recuerda leer las normas de la descripción.
+protegido por: 
+L̴͕̰̠̤̃̒̒ḛ̵̠̟̠͆̓̎n̴̘͈͊͛͗͝a̶̛͓͊ A̵n̴o̶n̷y̶m̸i̶s̴s̶          ⪻̤̈N̴͖e̵͠b̷̛̈́á̴̬̌l̶͐́o̸͡⪼̤̈
+⠀⠀⠀   ⠀⪻̤̈M̸̞̱̜͒̈͑̐̈́̇•̴̻̖̼͓͈̀̈́̈̓̍̔̚ͅT̴̤̩̀͑̄͗̒͑̕ḩ̸͇͔̑͒́͌̄e̷̙̱̎•̷̔̓̉̽̑̕̕H̶͔̰͑å̷͙̹̾͝͝ť̵̎͐́̏̒̈ë̶̛͖͓̙͎́́ṛ̷̊̊⪼̤̈`
 }
 exports.welcome = welcome
 
